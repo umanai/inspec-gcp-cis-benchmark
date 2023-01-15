f9b7f091854b30430c5d04bb279dba0d8df71725
Stijn Tratsaert <stijn.tratsaert.it@gmail.com>
2023-01-15 18:42:12 +0100

    print stuff

--- controls/2.06-logging.rb
+++ controls/2.06-logging.rb
@@ -51,8 +51,11 @@ control "cis-gcp-#{control_id}-#{control_abbrev}" do
   google_project_metrics(project: gcp_project_id).where(metric_filter: log_filter).metric_types.each do |metrictype|
     describe.one do
       filter = "metric.type=\"#{metrictype}\" resource.type=\"audited_resource\""
+      puts filter
       google_project_alert_policies(project: gcp_project_id).where(policy_enabled_state: true).policy_names.each do |policy|
         condition = google_project_alert_policy_condition(policy: policy, filter: filter)
+        puts condition
+        puts condition.exists?
         describe "[#{gcp_project_id}] Custom Role changes alert policy" do
           subject { condition }
           it { should exist }
